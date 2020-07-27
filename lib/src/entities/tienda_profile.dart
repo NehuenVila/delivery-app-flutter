cb33666e2b656620914c35c7fd3d4d6d0c3a38a7
Nehutrew <50326654+Nehuetrew@users.noreply.github.com>
2020-07-27 20:01:53 -0300

    chew sos un carro

---
+++ lib/src/entities/tienda_profile.dart
@@ -0,0 +1,14 @@
+
+class Tienda{
+  String id;
+  String nombre;
+  String logo;
+  String horario;
+  double rating;
+  String direccion;
+  String Descripcion;
+  String telefono;
+
+  Tienda( this.nombre, this.logo, this.horario, this.rating,
+      this.direccion, this.Descripcion, this.telefono);
+}
--- lib/src/pages/categorias_page.dart
+++ lib/src/pages/categorias_page.dart
@@ -1,3 +1,6 @@
+import 'dart:ffi';
+
+import 'package:delivery_prueba1/src/entities/tienda_profile.dart';
 import 'package:delivery_prueba1/src/utils/controller_util.dart';
 import 'package:flutter/material.dart';
 
@@ -5,37 +8,43 @@ import 'package:flutter/material.dart';
 
 class CategoriasPage extends StatelessWidget {
 
-  var Categoria = {
-    "titulo": "Favio",
-//    "img": "IMG"
-  };
+ List <Tienda> tiendas = [];
+
+ Tienda verduFavio = Tienda("Faviola", "assets/logos/logo.jpg", "de 15 a 19", 2.5,
+     "frente al messi", "Chew siempre me compra las bananas", "3544410861");
 
 
   @override
   Widget build(BuildContext context) {
+    tiendas.add(verduFavio);
     return Scaffold(
       appBar: AppBar(
         title: Text(Controller.categoria),
       ),
       body: Container(
-        child: _listaItems(Categoria, context),
-
-
+        child: ListView(
+          children: _listaItems(tiendas, context),
+        ),
       ),
     );
   }
 
-  List<Widget> _listaItems(Map data, BuildContext context) {
-    final Map opciones = {};
+
+  List<Widget> _listaItems(List<Tienda> data, BuildContext context) {
+    final List<Widget> listaux = [];
+
 
     data.forEach((item) {
       final widgetTemp = ListTile(
-        title: Text(item['titulo']),
-        onTap:() {},
+        title: Text(item.nombre),
+        leading: CircleAvatar(backgroundImage: AssetImage(item.logo), radius: 30,),
+        trailing: Icon(Icons.keyboard_arrow_right),
+        onTap: () {},
+        contentPadding: EdgeInsets.all(15),
       );
-      opciones..add(widgetTemp)
+      listaux..add(widgetTemp)
         ..add(Divider());
     });
-    return opciones;
+    return listaux;
   }
 }
